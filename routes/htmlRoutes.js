b5381c5c04410e0b716e9236fb01144ea398df70
Sean Bannon <seannbannon@gmail.com>
2021-05-27 13:46:17 -0500

    please

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -7,11 +7,13 @@ const router = require("express").Router();
 // ROUTING
 
 // HTML GET Request: Responds with the notes.html file
-router.get("/notes", (req, res) => res.sendFile(path.join(__dirname, "public/notes.html")));
+router.get("/", (req, res) => res.json(path.join(__dirname, "public/index.html")));
+
 
 
 // HTML GET Request: Added for Heroku
-router.get("/", (req, res) => res.json(path.join(__dirname, "public/index.html")));
+router.get("/notes", (req, res) => res.sendFile(path.join(__dirname, "public/notes.html")));
+
 
 
 // HTML GET Request: Responds with the index.html & all other file(s)
